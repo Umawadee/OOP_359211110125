c33a6854239468788bd64384b1abc56eeee46b1f
umawadee <ta-n2214@hotmail.com>
2017-11-17 11:15:07 +0700

    successfuly information data

--- src/oop_lab/s359211110125/rmutsv/com/myfirstJAVA.java
+++ src/oop_lab/s359211110125/rmutsv/com/myfirstJAVA.java
@@ -3,6 +3,9 @@ package oop_lab.s359211110125.rmutsv.com;
 public class myfirstJAVA {
     public static void main(String[] args) {
         System.out.println("Hello Wordl,I'm umawadee");
+        System.out.println("Information systsm");
+        System.out.println("Faculty of Mangement Technololgy");
+        System.out.println("rmutsv");
     }
 }
 
