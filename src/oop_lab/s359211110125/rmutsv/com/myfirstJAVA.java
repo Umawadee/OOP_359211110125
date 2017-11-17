317216a4882042e7bf1bcdda2a024bc216f28a5e
unknown <ta-n2214@hotmail.com>
2017-11-17 10:33:02 +0700

    Initial commit

---
+++ src/oop_lab/s359211110125/rmutsv/com/myfirstJAVA.java
@@ -0,0 +1,8 @@
+package oop_lab.s359211110125.rmutsv.com;
+
+public class myfirstJAVA {
+    public static void main(String[] args) {
+        System.out.println("Hello Wordl,I'm umawadee");
+    }
+}
+
