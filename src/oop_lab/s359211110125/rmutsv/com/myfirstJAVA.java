61b8f4f54943341422fc8ba110172d13dc9b2337
umawadee <ta-n2214@hotmail.com>
2017-11-17 11:18:45 +0700

    Testing edit code

--- src/oop_lab/s359211110125/rmutsv/com/myfirstJAVA.java
+++ src/oop_lab/s359211110125/rmutsv/com/myfirstJAVA.java
@@ -4,8 +4,7 @@ public class myfirstJAVA {
     public static void main(String[] args) {
         System.out.println("Hello Wordl,I'm umawadee");
         System.out.println("Information systsm");
-        System.out.println("Faculty of Mangement Technololgy");
-        System.out.println("rmutsv");
+
     }
 }
 
