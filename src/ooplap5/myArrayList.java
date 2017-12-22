19a90bc195e9ab2d30af3389a358844af8fa2f33
LAB_COM-MT_1 <ta-n2214@hotmail.com>
2017-12-22 10:52:57 +0700

    myArrayList

---
+++ src/ooplap5/myArrayList.java
@@ -0,0 +1,29 @@
+package ooplap5;
+
+import java.util.ArrayList;
+
+public class myArrayList {
+    public static void main(String[] args) {
+        //ArrayList
+        ArrayList List = new ArrayList();
+        String name = "Tan";
+        List.add(name);
+        System.out.println(List);
+        List.add("Toey");
+        List.add("Tom");
+        System.out.println(List);
+        List.add(2, "ADAM");
+        System.out.println(List);
+        List.add(1);
+        System.out.println(List);
+        List.add(10.10);
+        System.out.println(List);
+        List.remove("Toey");
+        System.out.println(List);
+        List.remove(3);
+        System.out.println(List);
+        System.out.println(List.size());
+        System.out.println(List.get(2));
+        System.out.println(List.indexOf("ADAM"));
+    }//main
+}//class
