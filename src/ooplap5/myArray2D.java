aa7d39e3791515d6412f13c14799245fbed571f9
LAB_COM-MT_1 <ta-n2214@hotmail.com>
2017-12-22 10:28:19 +0700

    InputDataToArray2D

---
+++ src/ooplap5/myArray2D.java
@@ -0,0 +1,36 @@
+package ooplap5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class myArray2D {
+    public static void main(String[] args) throws IOException {
+        //Array 2D
+        int[][]number = new int[2][3];
+        number = InputData(number);
+        ShowData(number);
+
+    }//main
+
+    private static void ShowData(int[][] number) {
+        for (int i=0;i<number.length;i++){
+            for (int j=0;j<number[i].length;j++)
+                System.out.print(number[i][j]+" ");
+
+        }
+    }
+    private static int[][] InputData(int[][] number) throws IOException {
+        BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter an integer:");
+        for (int i=0;i<number.length;i++){
+            for (int j=0;j<number[i].length;j++){
+                System.out.print("number["+i+"]["+j+"]: ");
+                number[i][j] = Integer.parseInt(reader.readLine());
+            }
+        }
+        return number;
+    }
+}//class
