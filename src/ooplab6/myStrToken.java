f659852b74a668d1420d80e5e7e5e80e6ceb2875
LAB_COM-MT_1 <ta-n2214@hotmail.com>
2017-12-29 10:12:00 +0700

    myStrBuff & myStrToken

---
+++ src/ooplab6/myStrToken.java
@@ -0,0 +1,20 @@
+package ooplab6;
+
+import java.util.StringTokenizer;
+
+public class myStrToken {
+    public static void main(String[] args) {
+     //StringTokenizer
+        String msg = "Hello Welcome MT RMUTSV 2017";
+        StringTokenizer myToken = new StringTokenizer(msg);
+        //counToken
+        System.out.println(myToken.countTokens());
+        while (myToken.hasMoreTokens()){
+            StringBuffer Buffer = new StringBuffer(myToken.nextToken());
+            System.out.print(Buffer.reverse()+" ");
+        }
+
+
+
+    }//main
+}//class
