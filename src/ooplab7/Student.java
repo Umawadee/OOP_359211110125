d7f5097bbc895998767d74a1821a3c169c74d03d
LAB_COM-MT_1 <ta-n2214@hotmail.com>
2018-01-26 09:05:25 +0700

    Student

---
+++ src/ooplab7/Student.java
@@ -0,0 +1,71 @@
+package ooplab7;
+//1.Student ID
+//2.Name
+//3.person ID
+//4.Address
+//Access Control
+//public
+//private
+//protected
+public class Student {
+    //properties of Student class (private ความเป็นส่วนตัว)
+    private String id;
+    private String name;
+    private String pid;
+    private String address;
+    //construeter
+    //default construeter
+    public Student (){}
+    //create by own
+    public Student(String id, String n, String pid, String a){
+        //assign date to class properties
+        this.id = id;
+        this.name = n;
+        this.pid = pid;
+        this.address = a;
+    }
+    //getter and setter methods
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", pid='" + pid + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}//class
